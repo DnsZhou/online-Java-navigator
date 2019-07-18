c1e9f05a548db09d69da98fd950ca496cef935e4
Dennis Zhou <zt1020@yeah.net>
2019-07-18 16:59:33 +0100

    update version numbet for v1.2.1

--- src/main/java/uk/ac/ncl/cs/tongzhou/navigator/Main.java
+++ src/main/java/uk/ac/ncl/cs/tongzhou/navigator/Main.java
@@ -2,6 +2,8 @@ package uk.ac.ncl.cs.tongzhou.navigator;
 
 import uk.ac.ncl.cs.tongzhou.navigator.webservice.WebServiceRouter;
 
+import java.util.Date;
+
 /**
  * s
  *
@@ -12,12 +14,12 @@ import uk.ac.ncl.cs.tongzhou.navigator.webservice.WebServiceRouter;
 public class Main<T> {
 
     public static void main(String[] args) throws Exception {
-//        var startTime = new Date();
-//        System.out.println("Process Started at " + startTime);
-//        RepositoryWalker.processRepository();
-//        var endTime = new Date();
-//        System.out.println("Process Finished at " + endTime);
-//        System.out.println("Took " + ((double) (endTime.getTime() - startTime.getTime()) / 1000) + " seconds.");
+        var startTime = new Date();
+        System.out.println("Process Started at " + startTime);
+        RepositoryWalker.processRepository();
+        var endTime = new Date();
+        System.out.println("Process Finished at " + endTime);
+        System.out.println("Took " + ((double) (endTime.getTime() - startTime.getTime()) / 1000) + " seconds.");
 
         WebServiceRouter.runServer();
     }
